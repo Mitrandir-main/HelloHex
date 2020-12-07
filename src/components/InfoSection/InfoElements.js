57bdd1f1a685f3299c56318b6867ba3d5144e2ad
Mitrandir-main <dian.kumanov@abv.bg>
2020-12-07 18:44:11 +0200

    more responsive

--- src/components/InfoSection/InfoElements.js
+++ src/components/InfoSection/InfoElements.js
@@ -11,13 +11,18 @@ export const InfoContainer = styled.div`
 export const InfoWrapper = styled.div`
   display: grid;
   z-index: 1;
-  height: 860px;
+  height: 900px;
   width: 100%;
   max-width: 1100px;
   margin-right: auto;
   margin-left: auto;
   padding: 0 24px;
   justify-content: center;
+
+  @media screen and (max-width: 768px) {
+    height: auto;
+  }
+
 `;
 
 export const InfoRow = styled.div`
@@ -81,11 +86,12 @@ export const Subtitle = styled.p`
 
 export const ImgWrap = styled.div`
   max-width: 510px;
-  height: 90%;
+  height: 100%;
+
 `;
 
 export const Img = styled.img`
-  width: 93%;
+  width: 100%;
   margin: 0 0 10px 0;
   padding-right: 0;
 `;
